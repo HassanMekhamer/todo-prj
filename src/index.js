f6c056afe9a580d8684c3bebbe1c6d129ee17e08
Hassan Mekhamer <hassan.mekhamer.97@gmail.com>
2025-01-21 14:22:51 +0200

    Basic project setup with no code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./style.css";
+
+
+
+
+class Item {
+    constructor(title, description, dueDate, priority, notes) {
+        this.title = title;
+        this.description =  description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+    }
+}
+
+// const todos = 
